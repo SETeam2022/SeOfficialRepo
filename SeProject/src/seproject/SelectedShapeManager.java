047036e846d25151bcf078110d3fa5ce6515b2a5
nslookupalexios <a.guarini7@studenti.unisa.it>
2022-11-24 12:13:09 +0100

    Fixed bug where two shapes change their color together.

--- SeProject/src/seproject/SelectedShapeManager.java
+++ SeProject/src/seproject/SelectedShapeManager.java
@@ -1,4 +1,5 @@
 package seproject;
+
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Node;
@@ -11,29 +12,28 @@ import javafx.scene.shape.Shape;
 /**
  * This class allows to select a shape on the screen.
  */
-public class SelectedShapeManager extends Tool{
-    
+public class SelectedShapeManager extends Tool {
+
     private Shape selectedShape;
-    
+
     private final SimpleBooleanProperty shapeIsSelected;
-    
+
     public SelectedShapeManager(Pane paper, ObjectProperty<Color> strokeColorProperty, ObjectProperty<Color> fillColorProperty) {
         super(paper, strokeColorProperty, fillColorProperty);
         shapeIsSelected = new SimpleBooleanProperty(false);
     }
-    
-    /**  
+
+    /**
      * This function will be called after a click with the mouse on the paper it
      * will select a shape on the screen.
      *
      * @param event is the event that generated the call to this method
-    */
+     */
     @Override
-    public void onMousePressed(MouseEvent event){
-        this.selectedShape = null;
-        for(Node node : this.getPaper().getChildren()){
+    public void onMousePressed(MouseEvent event) {
+        for (Node node : this.getPaper().getChildren()) {
             Shape tmp = (Shape) node;
-            if (node.getBoundsInParent().contains(event.getX(),event.getY())){
+            if (node.getBoundsInParent().contains(event.getX(), event.getY())) {
                 DropShadow ds1 = new DropShadow();
                 ds1.setOffsetY(4.0f);
                 ds1.setOffsetX(2.0f);
@@ -45,17 +45,19 @@ public class SelectedShapeManager extends Tool{
                 tmp.fillProperty().unbind();
                 node.setEffect(null);
                 this.shapeIsSelected.setValue(false);
-                }
+            }
         }
     }
-        
+
     @Override
-    public void onMouseDragged(MouseEvent event){
-        if (this.getSelectedShape()!=null){
-            this.selectedShape.setLayoutX((event.getX()-selectedShape.getLayoutBounds().getMinX()));
-            this.selectedShape.setLayoutY((event.getY()-selectedShape.getLayoutBounds().getMinY()));
+    public void onMouseDragged(MouseEvent event) {
+        if (this.getSelectedShape() != null) {
+            this.selectedShape.setLayoutX((event.getX() - selectedShape.getLayoutBounds().getMinX()));
+            this.selectedShape.setLayoutY((event.getY() - selectedShape.getLayoutBounds().getMinY()));
         }
-    };
+    }
+
+    ;
     /**
     * 
     * @return selected shape
@@ -63,42 +65,44 @@ public class SelectedShapeManager extends Tool{
     public Shape getSelectedShape() {
         return selectedShape;
     }
-    
-    
+
     /**
-     * 
+     *
      * @param selectedShape set the selected shape
      */
     private void setSelectedShape(Shape selectedShape) {
+        if (this.selectedShape != null) {
+            this.selectedShape.strokeProperty().unbind();
+            this.selectedShape.fillProperty().unbind();
+        }
         this.selectedShape = selectedShape;
         this.getFillColorProperty().setValue(Color.valueOf(this.selectedShape.getFill().toString()));
         this.getStrokeColorProperty().setValue(Color.valueOf(this.selectedShape.getStroke().toString()));
         this.selectedShape.strokeProperty().bind(this.getStrokeColorProperty());
         this.selectedShape.fillProperty().bind(this.getFillColorProperty());
     }
-    
+
     /**
      * @return shapeIsSelected an observable property that is true if there is
      * something selected else is fase
      */
-    public SimpleBooleanProperty getShapeIsSelectedProperty(){
+    public SimpleBooleanProperty getShapeIsSelectedProperty() {
         return shapeIsSelected;
     }
-    
+
     /**
      * When this method is called if a shape has been selected it will be
      * deleted from the paper
      */
-    public void deleteSelectedShape(){
-        
-        if(this.selectedShape == null) return;
-   
+    public void deleteSelectedShape() {
+
+        if (this.selectedShape == null) {
+            return;
+        }
+
         this.getPaper().getChildren().remove(this.selectedShape);
         this.selectedShape = null;
         this.shapeIsSelected.setValue(false);
     }
-    
-    
-    
-    
+
 }
