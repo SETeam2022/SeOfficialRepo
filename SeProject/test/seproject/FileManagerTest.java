a1cd1aa265cbdca58baa6cbe7f6a264eeaa9b0b7
graziaFerrara <grazia.ferrara2000@gmail.com>
2022-11-26 12:09:44 +0100

    FileManager's tests improvement

--- SeProject/test/seproject/FileManagerTest.java
+++ SeProject/test/seproject/FileManagerTest.java
@@ -3,8 +3,6 @@ package seproject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.layout.Pane;
@@ -12,103 +10,22 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+/**
+ * This class' aim is the testing of the save and load methods of the 
+ * FileManager class.
+ * 
+ */
 public class FileManagerTest {
     
     private FileManager fm;
     private Pane paper;
-    private String expectedXML, actualXML, javaVersion = System.getProperty("java.version");
-    final private String out1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-    "<java version=\""+ javaVersion +"\" class=\"java.beans.XMLDecoder\">" +
-    " <array class=\"javafx.scene.Node\" length=\"0\"/>" +
-    "</java>",
-    out2 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-    "<java version=\""+ javaVersion +"\" class=\"java.beans.XMLDecoder\">" +
-    " <array class=\"javafx.scene.Node\" length=\"1\">" +
-    "  <void index=\"0\">" +
-    "   <object class=\"javafx.scene.shape.Rectangle\">" +
-    "    <void property=\"fill\">" +
-    "     <object class=\"javafx.scene.paint.Color\">" +
-    "      <double>1.0</double>" +
-    "      <double>0.7529411911964417</double>" +
-    "      <double>0.7960784435272217</double>" +
-    "      <double>1.0</double>" +
-    "     </object>" +
-    "    </void>" +
-    "    <void property=\"height\">" +
-    "     <double>2.0</double>" +
-    "    </void>" +
-    "    <void property=\"stroke\">" +
-    "     <object class=\"javafx.scene.paint.Color\">" +
-    "      <double>1.0</double>" +
-    "      <double>0.0</double>" +
-    "      <double>0.0</double>" +
-    "      <double>1.0</double>" +
-    "     </object>" +
-    "    </void>" +
-    "    <void property=\"width\">" +
-    "     <double>6.0</double>" +
-    "    </void>" +
-    "   </object>" +
-    "  </void>" +
-    " </array>" +
-    "</java>",
-    out3 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
-          "<java version=\""+ javaVersion +"\" class=\"java.beans.XMLDecoder\">"+
-          " <array class=\"javafx.scene.Node\" length=\"3\">"+
-          "  <void index=\"0\">   <object class=\"javafx.scene.shape.Rectangle\">"+
-          "    <void property=\"fill\">"+
-          "     <object class=\"javafx.scene.paint.Color\">"+
-          "      <double>1.0</double>"+
-          "      <double>0.7529411911964417</double>"+
-          "      <double>0.7960784435272217</double>"+
-          "      <double>1.0</double>"+
-          "     </object>"+
-          "    </void>"+
-          "    <void property=\"height\">"+
-          "     <double>2.0</double>"+
-          "    </void>"+
-          "    <void property=\"stroke\">"+
-          "     <object class=\"javafx.scene.paint.Color\">"+
-          "      <double>1.0</double>"+
-          "      <double>0.0</double>"+
-          "      <double>0.0</double>"+
-          "      <double>1.0</double>"+
-          "     </object>"+
-          "    </void>"+
-          "    <void property=\"width\">"+
-          "     <double>6.0</double>"+
-          "    </void>"+
-          "   </object>"+
-          "  </void>"+
-          "  <void index=\"1\">"+
-          "   <object class=\"javafx.scene.shape.Line\">"+
-          "    <void property=\"endX\">"+
-          "     <double>4.0</double>"+
-          "    </void>"+
-          "    <void property=\"endY\">"+
-          "     <double>4.0</double>"+
-          "    </void>"+
-          "   </object>"+
-          "  </void>"+
-          "  <void index=\"2\">"+
-          "   <object class=\"javafx.scene.shape.Ellipse\">"+
-          "    <void property=\"radiusX\">"+
-          "     <double>4.0</double>"+
-          "    </void>"+
-          "    <void property=\"radiusY\">"+
-          "     <double>2.0</double>"+
-          "    </void>"+
-          "   </object>"+
-          "  </void>"+
-          " </array></java>";
-            
-    private File file, file2, file3;
+    private File file1, file2, file3;
     
     public FileManagerTest() {
         
@@ -122,14 +39,16 @@ public class FileManagerTest {
     public TemporaryFolder folder = new TemporaryFolder();
     
     /**
-     * This will be executed before each test, temporary files are created
+     * This will be executed before each test, a Pane, a FileManager and all
+     * temporary files are created.
+     * 
      */
     @Before
     public void setUp(){
         paper = new Pane();
         fm = new FileManager(paper);
         try {
-            file = folder.newFile("testfile1.xml");
+            file1 = folder.newFile("testfile1.xml");
             file2 = folder.newFile("testfile2.xml");
             file3 = folder.newFile("testfile3.xml");
         } catch (IOException ex) {
@@ -143,46 +62,73 @@ public class FileManagerTest {
     @Test
     public void testSave() {
         try {
+            
             System.out.println("save");
-            fm.save(file);
             
             // Test 1: the pane has no shapes on it
-            expectedXML = out1;
+            fm.save(file1);
+            fm.load(file1); // Now the saved drawing has been loaded into the pane
+            assertEquals(paper.getChildren().toArray().length, 0);
             
-            Scanner scan = new Scanner(file);
-            actualXML = "";
-            while (scan.hasNext()) { actualXML += scan.nextLine();}
-            scan.close();
-            assertEquals(expectedXML, actualXML);
-            
-            // Test 2: the pane has a shape on it
+            // Test 2: the pane has a colored shape on it
             Rectangle rect = new Rectangle(0,0,6,2);
             rect.setFill(Color.PINK);
             rect.setStroke(Color.RED);
             paper.getChildren().add(rect);
-            fm.save(file);
-            
-            expectedXML = out2;
-            
-            scan = new Scanner(file);
-            actualXML = "";
-            while (scan.hasNext()) { actualXML += scan.nextLine();}
-            scan.close();
-            assertEquals(expectedXML, actualXML);
-            
+            fm.save(file2);
+            fm.load(file2); // Now the saved drawing has been loaded into the pane
+            // check the number of shapes into the paper    
+            assertEquals(paper.getChildren().toArray().length, 1);  
+            // check if the shape is a Rectangle
+            assertTrue(paper.getChildren().toArray()[0] instanceof Rectangle); 
+            Rectangle rect2 = (Rectangle) paper.getChildren().toArray()[0];
+            // check dims
+            assertEquals(rect2.getX(), rect.getX(), 0);
+            assertEquals(rect2.getY(), rect.getY(), 0);
+            assertEquals(rect2.getWidth(), rect.getWidth(), 0);
+            assertEquals(rect2.getHeight(), rect.getHeight(), 0);
+            // check colors
+            assertEquals(rect2.getFill(), rect.getFill());
+            assertEquals(rect2.getStroke(), rect.getStroke());
+                        
             // Test 3: the pane has multiple different shapes on it
             Line line = new Line(0,0,4,4);
             Ellipse ellipse = new Ellipse(0, 0,4, 2);
             paper.getChildren().addAll(line, ellipse);
-            fm.save(file);
-            
-            expectedXML = out3;
-            
-            scan = new Scanner(file);
-            actualXML = "";
-            while (scan.hasNext()) { actualXML += scan.nextLine();}
-            scan.close();
-            assertEquals(expectedXML, actualXML);
+            fm.save(file3);
+            fm.load(file3); // Now the saved drawing has been loaded into the pane
+            // check the number of shapes into the paper
+            assertEquals(paper.getChildren().toArray().length, 3); 
+            // check the instances' type
+            assertTrue(paper.getChildren().toArray()[0] instanceof Rectangle);
+            assertTrue(paper.getChildren().toArray()[1] instanceof Line);
+            assertTrue(paper.getChildren().toArray()[2] instanceof Ellipse);
+            rect2 = (Rectangle) paper.getChildren().toArray()[0];
+            // check rect dims
+            assertEquals(rect2.getX(), rect.getX(), 0);
+            assertEquals(rect2.getY(), rect.getY(), 0);
+            assertEquals(rect2.getWidth(), rect.getWidth(), 0);
+            assertEquals(rect2.getHeight(), rect.getHeight(), 0);
+            // check rect colors
+            assertEquals(rect2.getFill(), rect.getFill());
+            assertEquals(rect2.getStroke(), rect.getStroke());
+            Line line2 = (Line) paper.getChildren().toArray()[1];
+            // check line dims
+            assertEquals(line.getStartX(), line2.getStartX(),0);
+            assertEquals(line.getStartY(), line2.getStartY(),0);
+            assertEquals(line.getEndX(), line2.getEndX(),0);
+            assertEquals(line.getEndY(), line2.getEndY(),0);
+            // check line color
+            assertEquals(line.getStroke(), line2.getStroke());
+            Ellipse ellipse2 = (Ellipse) paper.getChildren().toArray()[2];
+            // check ell dims
+            assertEquals(ellipse2.getCenterX(), ellipse.getCenterX(),0);
+            assertEquals(ellipse2.getCenterY(), ellipse.getCenterY(),0);
+            assertEquals(ellipse2.getRadiusX(), ellipse.getRadiusX(),0);
+            assertEquals(ellipse2.getRadiusY(), ellipse.getRadiusY(),0);
+            // check ellipse's colors
+            assertEquals(ellipse2.getFill(), ellipse.getFill());
+            assertEquals(ellipse2.getStroke(), ellipse.getStroke());
             
         } catch (IOException ex) {
             Logger.getLogger(FileManagerTest.class.getName()).log(Level.SEVERE, null, ex);
@@ -197,26 +143,71 @@ public class FileManagerTest {
     public void testLoad() {
         System.out.println("load");
         try {
-            // Test 1: the loaded drawing has a pane with no children
-            PrintWriter pw = new PrintWriter(file);
-            pw.println(out1);
-            pw.close();
-            fm.load(file);
-            assertEquals(0, paper.getChildren().toArray().length);
             
-            // Test 2: the loaded drawing has a pane with just one child
-            pw = new PrintWriter(file2);
-            pw.println(out2);
-            pw.close();
-            fm.load(file2);
-            assertEquals(1, paper.getChildren().toArray().length);
+            // Test 1: the pane has no shapes on it
+            fm.save(file1);
+            fm.load(file1); // Now the saved drawing has been loaded into the pane
+            assertEquals(paper.getChildren().toArray().length, 0);
             
-            // Test 3: the loaded drawing has a pane with many children
-            pw = new PrintWriter(file3);
-            pw.println(out3);
-            pw.close();
-            fm.load(file3);
-            assertEquals(3, paper.getChildren().toArray().length);
+            // Test 2: the pane has a colored shape on it
+            Rectangle rect = new Rectangle(0,0,6,2);
+            rect.setFill(Color.PINK);
+            rect.setStroke(Color.RED);
+            paper.getChildren().add(rect);
+            fm.save(file2);
+            fm.load(file2); // Now the saved drawing has been loaded into the pane
+            // check the number of shapes into the paper     
+            assertEquals(paper.getChildren().toArray().length, 1); 
+            // check if the shape is a Rectangle
+            assertTrue(paper.getChildren().toArray()[0] instanceof Rectangle); 
+            Rectangle rect2 = (Rectangle) paper.getChildren().toArray()[0];
+            // check dims
+            assertEquals(rect2.getX(), rect.getX(), 0);
+            assertEquals(rect2.getY(), rect.getY(), 0);
+            assertEquals(rect2.getWidth(), rect.getWidth(), 0);
+            assertEquals(rect2.getHeight(), rect.getHeight(), 0);
+            // check colors
+            assertEquals(rect2.getFill(), rect.getFill());
+            assertEquals(rect2.getStroke(), rect.getStroke());
+                        
+            // Test 3: the pane has multiple different shapes on it
+            Line line = new Line(0,0,4,4);
+            Ellipse ellipse = new Ellipse(0, 0,4, 2);
+            paper.getChildren().addAll(line, ellipse);
+            fm.save(file3);
+            fm.load(file3); // Now the saved drawing has been loaded into the pane
+            // check the number of shapes into the paper
+            assertEquals(paper.getChildren().toArray().length, 3); 
+            // check the instances' type
+            assertTrue(paper.getChildren().toArray()[0] instanceof Rectangle);
+            assertTrue(paper.getChildren().toArray()[1] instanceof Line);
+            assertTrue(paper.getChildren().toArray()[2] instanceof Ellipse);
+            rect2 = (Rectangle) paper.getChildren().toArray()[0];
+            // check rect dims
+            assertEquals(rect2.getX(), rect.getX(), 0);
+            assertEquals(rect2.getY(), rect.getY(), 0);
+            assertEquals(rect2.getWidth(), rect.getWidth(), 0);
+            assertEquals(rect2.getHeight(), rect.getHeight(), 0);
+            // check rect colors
+            assertEquals(rect2.getFill(), rect.getFill());
+            assertEquals(rect2.getStroke(), rect.getStroke());
+            Line line2 = (Line) paper.getChildren().toArray()[1];
+            // check line dims
+            assertEquals(line.getStartX(), line2.getStartX(),0);
+            assertEquals(line.getStartY(), line2.getStartY(),0);
+            assertEquals(line.getEndX(), line2.getEndX(),0);
+            assertEquals(line.getEndY(), line2.getEndY(),0);
+            // check line color
+            assertEquals(line.getStroke(), line2.getStroke());
+            Ellipse ellipse2 = (Ellipse) paper.getChildren().toArray()[2];
+            // check ell dims
+            assertEquals(ellipse2.getCenterX(), ellipse.getCenterX(),0);
+            assertEquals(ellipse2.getCenterY(), ellipse.getCenterY(),0);
+            assertEquals(ellipse2.getRadiusX(), ellipse.getRadiusX(),0);
+            assertEquals(ellipse2.getRadiusY(), ellipse.getRadiusY(),0);
+            // check ellipse's colors
+            assertEquals(ellipse2.getFill(), ellipse.getFill());
+            assertEquals(ellipse2.getStroke(), ellipse.getStroke());
             
         } catch (FileNotFoundException ex) {
             Logger.getLogger(FileManagerTest.class.getName()).log(Level.SEVERE, null, ex);
