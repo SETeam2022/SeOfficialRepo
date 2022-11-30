ddedb2d4e3bba4bc3f4e8181b05d893532fc79e2
teodo <teodo@192.168.56.1>
2022-11-30 20:00:35 +0100

    Added and tested the DeleteShapeCommand

---
+++ SeProject/src/seproject/commands/DeleteShapeCommand.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seproject.commands;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Shape;
+import seproject.tools.SelectedShapeManager;
+
+/**
+ *
+ * @author teodo
+ */
+public class DeleteShapeCommand implements Command {
+    
+    private Shape shape;
+    
+    private Pane paper;
+    
+    public DeleteShapeCommand(Shape shape, Pane paper){
+        this.shape = shape;
+        this.paper = paper;
+    }
+
+    @Override
+    public void execute() {
+        paper.getChildren().remove(shape);
+    }
+
+    @Override
+    public void undo() {
+        paper.getChildren().add(shape);
+    }
+    
+}
---
+++ SeProject/test/seproject/commands/DeleteShapeCommandTest.java
@@ -0,0 +1,58 @@
+package seproject.commands;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author teodo
+ */
+public class DeleteShapeCommandTest {
+    
+    private Pane paper;
+    
+    private Shape testShape;
+    
+    private DeleteShapeCommand com;
+    
+    public DeleteShapeCommandTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        paper = new Pane();
+        testShape = new Rectangle();
+        com = new DeleteShapeCommand(testShape,paper);
+    }
+    
+    /**
+     * The method execute the command prepared in the setUp and tests if the
+     * thestShape is between the children of the pane
+     */
+    @Test
+    public void testExecute() {
+        System.out.println("execute");
+        com.execute();
+        assertTrue(!paper.getChildren().contains(testShape));
+    }
+
+    /**
+     * The method execute the undo of the command prepared in the setUp and tests if the
+     * thestShape is removed from the children of the pane
+     */
+    @Test
+    public void testUndo() {
+        System.out.println("undo");
+        com.execute();
+        com.undo();
+        assertTrue(paper.getChildren().contains(testShape));
+    }
+    
+}
