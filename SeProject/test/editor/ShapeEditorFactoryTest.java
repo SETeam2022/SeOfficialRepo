3a04023f01481dcbd7f7d80dd0363dcccec1a971
nslookupalexios <a.guarini7@studenti.unisa.it>
2022-12-02 16:10:36 +0100

    ShapeEditorFactoryTest class implemented

--- SeProject/test/editor/ShapeEditorFactoryTest.java
+++ SeProject/test/editor/ShapeEditorFactoryTest.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package editor;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -19,26 +16,27 @@ public class ShapeEditorFactoryTest {
     public ShapeEditorFactoryTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     @Test
     public void testGetInstance() {
         System.out.println("getInstance");
-        Class shapeClass = null;
-        ShapeEditor expResult = null;
-        ShapeEditor result = ShapeEditorFactory.getInstance(shapeClass);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        Class<?> expectedClass; 
+        Class<?> actualClass;
+        Shape testShape;
+        // Test 1
+        expectedClass = RectangleEditor.class;
+        testShape = new Rectangle();
+        actualClass = ShapeEditorFactory.getInstance(testShape.getClass()).getClass();
+        assertEquals(expectedClass,actualClass);
+        // Test 2
+        expectedClass = LineEditor.class;
+        testShape = new Line();
+        actualClass = ShapeEditorFactory.getInstance(testShape.getClass()).getClass();
+        assertEquals(expectedClass,actualClass);
+        // Test 3
+        expectedClass = EllipseEditor.class;
+        testShape = new Ellipse();
+        actualClass = ShapeEditorFactory.getInstance(testShape.getClass()).getClass();
+        assertEquals(expectedClass,actualClass);
     }
     
 }
