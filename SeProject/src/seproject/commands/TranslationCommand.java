dc5d866273caa67fcb1bf300fce630bdfe754a35
teodo <teodo@192.168.56.1>
2022-12-03 11:51:30 +0100

    update translation command with scale factor

--- SeProject/src/seproject/commands/TranslationCommand.java
+++ SeProject/src/seproject/commands/TranslationCommand.java
@@ -6,22 +6,24 @@ import javafx.scene.shape.Shape;
 public class TranslationCommand implements Command {
 
     private final Shape shape;
-    private final double posX, posY, startX, startY;
+    private final double posX, posY, startX, startY, scaleX, scaleY;
     private final MouseEvent event;
 
-    public TranslationCommand(Shape shape, double posX, double posY, double startX, double startY, MouseEvent event) {
+    public TranslationCommand(Shape shape ,double posX, double posY, double startX, double startY,double scaleX,double scaleY,MouseEvent event) {
         this.shape = shape;
         this.posX = posX;
         this.posY = posY;
         this.startX = startX;
         this.startY = startY;
         this.event = event;
+        this.scaleX = scaleX;
+        this.scaleY = scaleY;
     }
 
     @Override
     public void execute() {
-        shape.setTranslateX(event.getSceneX() - posX);
-        shape.setTranslateY(event.getSceneY() - posY);
+        shape.setTranslateX(event.getSceneX()/scaleX - posX);
+        shape.setTranslateY(event.getSceneY()/scaleY - posY);
     }
 
     @Override
@@ -29,5 +31,4 @@ public class TranslationCommand implements Command {
         shape.setTranslateX(startX);
         shape.setTranslateY(startY);
     }
-
 }
