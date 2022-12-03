01d853d6a145d87f8eaa36942aeaa95a3e66b11a
nslookupalexios <a.guarini7@studenti.unisa.it>
2022-12-03 18:53:34 +0100

    Refactoring EditorTest package.

--- SeProject/src/editor/RectangleEditor.java
+++ SeProject/src/editor/RectangleEditor.java
@@ -5,26 +5,51 @@ import javafx.scene.shape.Shape;
 
 public class RectangleEditor implements ShapeEditor {
 
+    /**
+     *
+     * @param shape the rectangle you want to edit Width
+     * @param width
+     */
     @Override
     public void setWidth(Shape shape, double width) {
         ((Rectangle) shape).setWidth(width);
     }
 
+    /**
+     *
+     * @param shape the rectangle you want to edit Height
+     * @param height
+     */
     @Override
     public void setHeight(Shape shape, double height) {
         ((Rectangle) shape).setHeight(height);
     }
 
+    /**
+     *
+     * @param shape the rectangle you want to get Width
+     * @return
+     */
     @Override
     public double getWidth(Shape shape) {
         return ((Rectangle) shape).getWidth();
     }
 
+    /**
+     *
+     * @param shape the rectangle you want to get Height
+     * @return
+     */
     @Override
     public double getHeight(Shape shape) {
         return ((Rectangle) shape).getHeight();
     }
 
+    /**
+     *
+     * @param shape the rectangle you want to clone
+     * @return the cloned rectangle
+     */
     @Override
     public Shape clone(Shape shape) {
         Rectangle original = (Rectangle) shape;
