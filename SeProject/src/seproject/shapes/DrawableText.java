0f47a27ad9137d792ffc5b32defeadb24c4a77c5
nslookupalexios <a.guarini7@studenti.unisa.it>
2022-12-09 10:31:30 +0100

    DrawableText class created.

--- SeProject/src/seproject/shapes/DrawableText.java
+++ SeProject/src/seproject/shapes/DrawableText.java
@@ -12,7 +12,6 @@ public class DrawableText extends Text implements DrawableShape{
 
     @Override
     public void setShapeWidth(double width) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
