8f44876ae529c4b8d2362b1b14abbc58e440fba9
Grazia Ferrara <g.ferrara75@studenti.unisa.it>
2022-12-10 11:14:42 +0100

    insertion of other documentation

--- SeProject/src/seproject/tools/DrawingTool.java
+++ SeProject/src/seproject/tools/DrawingTool.java
@@ -4,6 +4,10 @@ import javafx.beans.property.ObjectProperty;
 import javafx.scene.paint.Color;
 import seproject.customComponents.DrawingArea;
 
+/**
+ * This class wants to represent the generic drawing tool.
+ * 
+ */
 public abstract class DrawingTool extends Tool {
 
     static final double widthStroke = 2.5;
@@ -11,16 +15,33 @@ public abstract class DrawingTool extends Tool {
     private final ObjectProperty<Color> strokeColorProperty;
     private final ObjectProperty<Color> fillColorProperty;
 
+    /**
+     * Drawing Tool creation.
+     * 
+     * @param paper is the pane on which the new shape nodes will be added
+     * @param strokeColorProperty is the associated ObjectProperty of Stroke
+     * Interior Picker's value.
+     * @param fillColorProperty is the associated ObjectProperty of Fill
+     * Interior Picker's value.
+     */
     public DrawingTool(DrawingArea paper, ObjectProperty<Color> strokeColorProperty, ObjectProperty<Color> fillColorProperty) {
         super(paper);
         this.strokeColorProperty = strokeColorProperty;
         this.fillColorProperty = fillColorProperty;
     }
 
+    /**
+     * 
+     * @return the strokeColorProperty
+     */
     public ObjectProperty<Color> getStrokeColorProperty() {
         return strokeColorProperty;
     }
 
+    /**
+     * 
+     * @return the fillColorProperty
+     */
     public ObjectProperty<Color> getFillColorProperty() {
         return fillColorProperty;
     }
