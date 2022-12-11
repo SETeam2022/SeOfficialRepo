53b3d9a1ba0c321051edd3a149590099adb7cb1a
nslookupalexios <a.guarini7@studenti.unisa.it>
2022-12-11 18:49:28 +0100

    Refacotring Constants TextToolText now it is okay.

--- SeProject/test/seproject/editor/ShapeEditorFactoryTest.java
+++ SeProject/test/seproject/editor/ShapeEditorFactoryTest.java
@@ -1,9 +1,13 @@
 package seproject.editor;
 
+import editor.EditorNotFoundException;
 import editor.EllipseEditor;
 import editor.LineEditor;
 import editor.RectangleEditor;
+import editor.ShapeEditor;
 import editor.ShapeEditorFactory;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.CubicCurve;
 
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
@@ -41,6 +45,47 @@ public class ShapeEditorFactoryTest {
         testShape = new Ellipse();
         actualClass = ShapeEditorFactory.getInstance(testShape.getClass()).getClass();
         assertEquals(expectedClass, actualClass);
+    }
+
+    /**
+     * Checks whether the getInstance method throws an EditorNotFoundException
+     * when the ShapeEditor instance for that specific Shape is not found.
+     *
+     */
+    @Test(expected = EditorNotFoundException.class)
+    public void testGetInstanceException() {
+        System.out.println("getInstanceException");
+        ShapeEditorFactory.getInstance(Arc.class);
+    }
+
+    @Test
+    public void testAddInstance() {
+        System.out.println("addInstance");
+
+        ShapeEditorFactory.addInstance(CubicCurve.class, new ShapeEditor<CubicCurve>() {
+            @Override
+            public void setWidth(CubicCurve shape, double width) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+
+            @Override
+            public void setHeight(CubicCurve shape, double height) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+
+            @Override
+            public double getWidth(CubicCurve shape) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+
+            @Override
+            public double getHeight(CubicCurve shape) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        });
+
+        ShapeEditorFactory.getInstance(CubicCurve.class);
+        
     }
 
 }
